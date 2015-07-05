e66276ab8180b140137138f11dcb339d918f388b
Alex <aleksandr.velichko.02@gmail.com>
2015-07-05 21:04:59 +0300

    12

--- src/main/java/ua/kiev/prog/MyPdfConverter.java
+++ src/main/java/ua/kiev/prog/MyPdfConverter.java
@@ -28,9 +28,9 @@ public class MyPdfConverter {
 		
 		
 		//final String TMP_DIR = System.getProperty("java.io.tmpdir");
-		BaseFont bf = BaseFont.createFont("fonts/free/oldstandard/OldStandard-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
-		Font font = new Font(bf);
-		
+		//BaseFont bf = BaseFont.createFont("fonts/free/oldstandard/OldStandard-Regular.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+		//Font font = new Font(bf);
+		Font font =  FontFactory.getFont("arial", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 		
 		//String FONTPATH = "/usr/share/fonts/truetype/msttcorefonts/ARIAL.TTF";
 		//Font font = FontFactory.getFont(FONTPATH, "UTF-8", BaseFont.EMBEDDED);
